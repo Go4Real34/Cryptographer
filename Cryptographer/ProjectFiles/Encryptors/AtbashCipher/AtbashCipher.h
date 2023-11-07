5dcccf2fdb385205a42432332f5c5a5ebf9a1c00
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-08 00:05:35 +0300

    add necessary AtbashCipher class function declarations

--- Cryptographer/ProjectFiles/Encryptors/AtbashCipher/AtbashCipher.h
+++ Cryptographer/ProjectFiles/Encryptors/AtbashCipher/AtbashCipher.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <string>
+
 #include "../Encryptor.h"
 
 class AtbashCipher : public Encryptor {
+	public:
+		AtbashCipher(const std::string& PlainText);
+		~AtbashCipher();
+
+		void setPlainText(const std::string& newPlainText) override;
+
+		std::string encrypt() override;
+		std::string decrypt() override;
 };
