e781d4f13702aee43ef0d21c92e61abdd68fc867
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-23 20:48:53 +0300

    add necessary CaesarCipher class function declarations

--- Cryptographer/ProjectFiles/Encryptors/CaesarCipher/CaesarCipher.h
+++ Cryptographer/ProjectFiles/Encryptors/CaesarCipher/CaesarCipher.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <string>
+
 #include "../Encryptor.h"
 
 class CaesarCipher : public Encryptor {
+	public:
+		CaesarCipher(const std::string& PlainText, uint8_t ShiftAmount);
+		~CaesarCipher();
+
+		void setPlainText(const std::string& newPlainText) override;
+		void setShiftAmount(uint8_t newShiftAmount);
+
+		std::string encrypt() override;
+		std::string decrypt() override;
+
+	private:
+		uint8_t shiftAmount = 0;
 };
