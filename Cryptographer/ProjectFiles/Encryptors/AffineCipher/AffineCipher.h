ac7f2e6ad819310eed70c0269bd961a492dfca8a
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-11-24 05:09:41 +0300

    add necessary AffineCipher class function declarations

--- Cryptographer/ProjectFiles/Encryptors/AffineCipher/AffineCipher.h
+++ Cryptographer/ProjectFiles/Encryptors/AffineCipher/AffineCipher.h
@@ -3,4 +3,21 @@
 #include "../Encryptor.h"
 
 class AffineCipher : public Encryptor {
+	public:
+		AffineCipher(const std::string& PlainText, const long long& Alpha, const long long& Beta);
+		~AffineCipher();
+
+		void setPlainText(const std::string& newPlainText) override;
+		void setAlpha(const long long& newAlpha);
+		void setBeta(const long long& newBeta);
+
+		std::string encrypt() override;
+		std::string decrypt() override;
+
+	private:
+		long long alpha = 1;
+		long long beta = 0;
+
+		char encryptCharacter(const char& character) const override;
+		char decryptCharacter(const char& character) const override;
 };
