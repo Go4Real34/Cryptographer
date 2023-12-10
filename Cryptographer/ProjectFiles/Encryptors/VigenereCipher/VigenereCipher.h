f6700eb9fe2e4e033559ceb6d881906a5412307d
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-10 14:09:27 +0300

    add necessary VigenereCipher class function declarations

--- Cryptographer/ProjectFiles/Encryptors/VigenereCipher/VigenereCipher.h
+++ Cryptographer/ProjectFiles/Encryptors/VigenereCipher/VigenereCipher.h
@@ -3,4 +3,20 @@
 #include "../Encryptor.h"
 
 class VigenereCipher : public Encryptor {
+	public:
+		VigenereCipher(const std::string& PlainText, const std::string& Key);
+		~VigenereCipher();
+
+		void setPlainText(const std::string& newPlainText) override;
+		void setKey(const std::string& newKey);
+
+		std::string encrypt() override;
+		std::string decrypt() override;
+
+	private:
+		std::string key = "";
+		size_t keySize = 0;
+
+		char encryptCharacter(const char& textCharacter, const char& keyCharacter);
+		char decryptCharacter(const char& textCharacter, const char& keyCharacter);
 };
