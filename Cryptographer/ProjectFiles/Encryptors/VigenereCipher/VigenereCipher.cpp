0d9f61e29477595f9c1e04c0a3a1f3fd9dc0d6d1
Go4Real34 <gorkemsarikaya2017@gmail.com>
2023-12-10 14:09:48 +0300

    add VigenereClass initialization function implementations

--- Cryptographer/ProjectFiles/Encryptors/VigenereCipher/VigenereCipher.cpp
+++ Cryptographer/ProjectFiles/Encryptors/VigenereCipher/VigenereCipher.cpp
@@ -1,1 +1,30 @@
 #include "VigenereCipher.h"
+
+VigenereCipher::VigenereCipher(const std::string& PlainText, const std::string& Key) {
+	this -> plainText = PlainText;
+	this -> textSize = PlainText.size();
+	this -> key = Key;
+	this -> keySize = Key.size();
+
+	this -> encryptedText = this -> encrypt();
+	this -> decryptedText = this -> decrypt();
+}
+
+VigenereCipher::~VigenereCipher() {
+}
+
+void VigenereCipher::setPlainText(const std::string& newPlainText) {
+	this -> plainText = newPlainText;
+	this -> textSize = newPlainText.size();
+
+	this -> encryptedText = this -> encrypt();
+	this -> decryptedText = this -> decrypt();
+}
+
+void VigenereCipher::setKey(const std::string& newKey) {
+	this -> key = newKey;
+	this -> keySize = newKey.size();
+
+	this -> encryptedText = this -> encrypt();
+	this -> decryptedText = this -> decrypt();
+}
